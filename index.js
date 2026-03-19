d279ddd402f5e6364ef1d5ab54d3522d18581360
Sanjay <sanjusanjay06406@gmail.com>
2026-03-19 22:46:35 +0530

    Working of CI

---
+++ index.js
@@ -0,0 +1,1 @@
+console.log("Learining CI/CD Pipelines")
---
+++ test.js
@@ -0,0 +1,3 @@
+console.log("Starting Tests....")
+setTimeout(()=> console.log("Test completes after 3 seconds"),3000)
+console.log("Tests Completed!")
